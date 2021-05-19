84c3eb26c06bcb102aa6bb088dfead54464c1146
master <master@rivne.itstep.org>
2021-05-19 17:10:40 +0300

    create element

--- 19.05.21/js/common.js
+++ 19.05.21/js/common.js
@@ -31,6 +31,57 @@ function GetCar() {
     CarsArr.forEach(car => {
         car.Print();
     })
-
+    RenderCars();
     console.log("Cars number: ", CarsArr.length);
 }
+
+function RenderCars() {
+    let root = document.getElementById("root");
+    let span = document.createElement("span");
+    span.setAttribute("class", "test");
+    span.innerHTML = CarsArr[0].Vendor;
+    root.appendChild(span);
+}
+
+
+
+
+// Expression
+// const Minus = function (a, b) {
+//     console.log(a - b)
+// }
+// // Minus(3, 1)
+
+// // Declaration
+// // Plus(5, 4);
+// function Plus(a, b) {
+//     console.log(a + b)
+// }
+
+// Arrow
+// const Division = (a, b) => {
+//     console.log(a / b)
+// }
+// Division(10, 2);
+// const Test = (a, b) => console.log(a / b)
+// Test(20, 5);
+
+// Annonymous
+// (function (a, b) {
+//     console.log(a * b)
+// }(5, 20))
+
+// function Dog(Name, Breed, Age) {
+//     this.Name = Name;
+//     this.Breed = Breed;
+//     this.Age = Age;
+//     this.Print = function () {
+//         console.log(`Name: ${this.Name}\nBreed: ${this.Breed}\nAge: ${this.Age}`)
+//     }
+// }
+
+// let Bobik = new Dog("bobik", "Hasky", 2);
+// Bobik.Print();
+
+// let tyson = new Dog("Tyson", "Taxa", 5);
+// tyson.Print();
